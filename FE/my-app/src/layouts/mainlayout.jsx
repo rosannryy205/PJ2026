f4eea2a2aefc571041d6edb2c66166ab5452f08a
rosannryy205 <khanhvmps39093@gmail.com>
2026-05-04 20:22:35 +0700

    sidebar

--- FE/my-app/src/layouts/mainlayout.jsx
+++ FE/my-app/src/layouts/mainlayout.jsx
@@ -1,17 +1,23 @@
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import SideBar from "../components/Sidebar";
 
 export default function MainLayout() {
-    return (
-        <>
-            <Header />
+  return (
+    <>
+      <Header />
 
-            <main style={{ minHeight: "80vh"}}>
-                <Outlet />
-            </main>
-            
-            <Footer />
-        </>
-    )
+      <div className="flex flex-1">
+        <aside className="w-64 bg-gray-100 p-4 border-none">
+            <SideBar />
+        </aside>
+        <main style={{ minHeight: "80vh" }}>
+          <Outlet />
+        </main>
+      </div>
+
+      <Footer />
+    </>
+  );
 }
